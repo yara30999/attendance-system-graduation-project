26910b02b4c50c622ca9a3dfdd65688cd27737af
yara30999 <yaranasserelniny@gmail.com>
2023-02-25 22:44:40 +0200

    initial files

---
+++ android/app/src/main/kotlin/com/example/fast_tende_doctor_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fast_tende_doctor_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DoctorApp());
+}
+
+class DoctorApp extends StatelessWidget {
+  const DoctorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
