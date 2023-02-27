c356bbd4cd0842a8ec61d7b64678dc62c8f7e2c5
yara30999 <yaranasserelniny@gmail.com>
2023-02-27 11:46:18 +0200

    add poppins font family , complete home blue container.

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
+const kSearchField = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+  hintText: 'Search class....',
+  hintStyle: TextStyle(
+      color: Color(0xff074e79), fontSize: 14.0, fontFamily: 'poppins'),
+  prefixIcon: Icon(Icons.search),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(12.0)),
+    borderSide: BorderSide.none,
+  ),
+);
 
 const kTextFieldDecoration = InputDecoration(
   hintText: 'Enter value',
@@ -12,6 +25,7 @@ const kTextFieldDecoration = InputDecoration(
     borderSide: BorderSide(color: Colors.grey, width: 1.0),
   ),
   focusedBorder: UnderlineInputBorder(
-    borderSide: BorderSide(color: Color.fromARGB(255, 114, 114, 114), width: 1.5),
+    borderSide:
+        BorderSide(color: Color.fromARGB(255, 114, 114, 114), width: 1.5),
   ),
 );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,21 +1,112 @@
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
-  static String id ='home_screen';
+  static String id = 'home_screen';
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late final dynamic currentHours;
+  late final dynamic currentMinutes;
+
+  @override
+  void initState() {
+    super.initState();
+    currentHours = DateTime.now().hour;
+    currentMinutes = DateTime.now().minute;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Center(),
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.only(top: 40.0,left:30.0,right: 30.0,bottom: 20.0 ),
+            decoration: const BoxDecoration(
+              color: Color(0xff0d8ad5),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(33.0),
+                bottomRight: Radius.circular(33.0),
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '$currentHours:$currentMinutes',
+                      style: const TextStyle(
+                        fontSize: 29.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    const ClipRRect(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20.0),
+                      ),
+                      child: Image(
+                        image: AssetImage('images/user1.png'),
+                        fit: BoxFit.cover,
+                        height: 56.3,
+                        width: 57.6,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20.0),
+                const Text(
+                  'Welcome Back',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w500),
+                ),
+                const SizedBox(height: 12.0),
+                const Text(
+                  'Let\'s find',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 36.0,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w600),
+                ),
+                const Text(
+                  'Your next class!',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 36.0,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w600),
+                ),
+                const SizedBox(height: 36.0),
+                ListView(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  children: [
+                    TextField(
+                      decoration: kSearchField,
+                      onChanged: (value){},
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 }
+
+
